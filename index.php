8f0dbc1498019a55cd9e2e6c21d813ab47a5bc94
lidia rubith <lidia rubith@gmail.com>
2016-02-29 20:29:45 -0500

    milagros

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>milagros</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+<marquee>
+<h1>milagros espimoza terraza</h1>
+</marquee>
+	<img src="1.jpg" width="400" alt="500">
+	<img src="2.jpg" width="400" alt="500">
+	<marquee>
+	<h2>milagros espinoza terraza</h2>
+	</marquee>
+</body>
+</html>
